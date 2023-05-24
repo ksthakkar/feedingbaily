77a2ccec330edb3f5f6176cae6991f0a1bde971b
Cahil1 <ahil.chitrarasu@gmail.com>
2023-05-24 13:53:31 -0700

    add titlescreen package

--- core/src/com/mygdx/game/HealthBar.java
+++ core/src/com/mygdx/game/HealthBar.java
@@ -1,4 +1,51 @@
 package com.mygdx.game;
+import com.badlogic.drop.Drop;
+import com.badlogic.drop.GameScreen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
-public class HealthBar {
+
+public class HealthBar implements Screen{
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
